a3e0af98cba93f3acc8eb977c96f9ce822756a00
jrs1lva <adailtonjunior0604@gmail.com>
2026-05-12 17:05:51 -0300

    feat: initializing calculadoraStore

--- agilemovies/src/main/java/com/jr/agilemovies/service/CalculadoraScore.java
+++ agilemovies/src/main/java/com/jr/agilemovies/service/CalculadoraScore.java
@@ -1,5 +1,26 @@
 package com.jr.agilemovies.service;
 
-public class CalculadoraScore {
+import com.jr.agilemovies.model.Filme;
 
+public class CalculadoraScore {
+	
+	private Filme filme;
+	private double total;
+	private double compatibilidadeGenero;
+	private double compatibilidadeDuracao;
+	private double popularidade;
+	private double afinidade;
+	
+	public CalculadoraScore(double compatibilidadeGenero, double compatibilidadeDuracao,
+			double popularidade, double afinidade) {
+		
+		this.compatibilidadeGenero = compatibilidadeGenero;
+		this.compatibilidadeDuracao = compatibilidadeDuracao;
+		this.popularidade = popularidade;
+		this.afinidade = afinidade;
+		
+	}
+	
+	
+	
 }
