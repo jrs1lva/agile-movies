60ca15d955a53d14022dae633b70d9799c8a607a
jrs1lva <adailtonjunior0604@gmail.com>
2026-05-12 11:04:49 -0300

    feat: finish class PerfilCinefilo

--- agilemovies/src/main/java/com/jr/agilemovies/model/PerfilCinefiloModel.java
+++ agilemovies/src/main/java/com/jr/agilemovies/model/PerfilCinefiloModel.java
@@ -1,5 +1,7 @@
 package com.jr.agilemovies.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,85 +30,91 @@ public class PerfilCinefiloModel {
 	private List<String> filmesAssistidos;
 	private Map<String, Integer> notas;
 	
-	public PerfilCinefiloModel(Map<Genero, Double> pesosGenero, int duracaoMinima, int duracaoMaxima,
-			ClassificacaoEtaria classificacaoMaxima, List<Idioma> idiomasAceitos, List<String> filmesAssistidos,
-			Map<String, Integer> notas) {
+	public PerfilCinefiloModel(int duracaoMinima, int duracaoMaxima, ClassificacaoEtaria classificacaoMaxima) {
 		
-		this.pesosGenero = pesosGenero;
-		this.duracaoMinima = duracaoMinima;
-		this.duracaoMaxima = duracaoMaxima;
+		definirDuracao(duracaoMinima, duracaoMaxima);
+		
 		this.classificacaoMaxima = classificacaoMaxima;
-		this.idiomasAceitos = idiomasAceitos;
-		this.filmesAssistidos = filmesAssistidos;
-		this.notas = notas;
+		
+		this.pesosGenero = new HashMap<Genero, Double>();
+		this.idiomasAceitos = new ArrayList<Idioma>();
+		this.filmesAssistidos = new ArrayList<String>();
+		this.notas = new HashMap<String, Integer>();
 		
 	}
 	
 	private void validarPeso(double peso) {
-		
 		if (peso < 0 || peso > 1) {
 			throw new PesoInvalidoException("O peso deve estar entre 0 e 1.");
 		}
 	}
 	
 	private void validarDuracao(int duracaoMinima, int duracaoMaxima) {
-		
 		if (duracaoMinima > duracaoMaxima) {
 			throw new DuracaoInvalidaException("A duração mínima deve ser menor que a duração máxima.");
 		}
-		
 	}
 	
 	private void validarNota(int nota) {
-		
 		if (nota < 1 || nota > 5) {
 			throw new NotaInvalidaException("A nota deve estar entre 1 e 5.");
 		}
-		
 	}
 	
-	public Map<Genero, Double> getPesosGenero() {
-		return pesosGenero;
+	public void adicionarIdioma(Idioma idioma) {
+		idiomasAceitos.add(idioma);
 	}
-	public void setPesosGenero(Map<Genero, Double> pesosGenero) {
-		this.pesosGenero = pesosGenero;
+	
+	public void adicionarNota(int nota, String filmeId) {
+		validarNota(nota);
+		notas.put(filmeId, nota);
 	}
+	
+	public void adicionarFilmeAssistido(String filmeId) {
+		filmesAssistidos.add(filmeId);
+	}
+	
+	public void definirDuracao(int duracaoMinima, int duracaoMaxima) {
+		validarDuracao(duracaoMinima, duracaoMaxima);
+		this.duracaoMinima = duracaoMinima;
+		this.duracaoMaxima = duracaoMaxima;
+	}
+	
+	public void definirPesoGenero(Genero genero, double peso) {
+		validarPeso(peso);
+		pesosGenero.put(genero, peso);
+	}
+	
+	// SETS AND GETTERS
+	
 	public int getDuracaoMinima() {
 		return duracaoMinima;
 	}
-	public void setDuracaoMinima(int duracaoMinima) {
-		this.duracaoMinima = duracaoMinima;
-	}
+	
 	public int getDuracaoMaxima() {
 		return duracaoMaxima;
 	}
-	public void setDuracaoMaxima(int duracaoMaxima) {
-		this.duracaoMaxima = duracaoMaxima;
-	}
+		
 	public ClassificacaoEtaria getClassificacaoMaxima() {
 		return classificacaoMaxima;
 	}
-	public void setClassificacaoMaxima(ClassificacaoEtaria classificacaoMaxima) {
-		this.classificacaoMaxima = classificacaoMaxima;
+	
+	public Map<Genero, Double> getPesosGenero() {
+		return new HashMap<>(pesosGenero);
 	}
+	
 	public List<Idioma> getIdiomasAceitos() {
-		return idiomasAceitos;
+		return new ArrayList<>(idiomasAceitos);
 	}
-	public void setIdiomasAceitos(List<Idioma> idiomasAceitos) {
-		this.idiomasAceitos = idiomasAceitos;
-	}
+
 	public List<String> getFilmesAssistidos() {
-		return filmesAssistidos;
+		return new ArrayList<>(filmesAssistidos);
 	}
-	public void setFilmesAssistidos(List<String> filmesAssistidos) {
-		this.filmesAssistidos = filmesAssistidos;
-	}
+
 	public Map<String, Integer> getNotas() {
-		return notas;
+		return new HashMap<>(notas);
 	}
-	public void setNotas(Map<String, Integer> notas) {
-		this.notas = notas;
-	}
+
 	
 	
 	
